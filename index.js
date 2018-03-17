6005ffbd088b28ba3a1daabe625dbe08a3b280ed
jsvilchis <vil15009@byui.edu>
2018-03-17 02:48:19 -0400

    trying to get the ajax to work

--- index.js
+++ index.js
@@ -114,6 +114,33 @@ function calculateRate(req, res) {
 }
 
 
+
+var url = 'https://api.nasa.gov/planetary/apod?api_key=ssJOmcyAlMslRMBklollwmpbUSmejdcgJlsemAzo';
+var httpRequest; //declare here for good scope
+if (window.XMLHttpRequest) { // Mozilla, Safari, IE7+ ...
+    httpRequest = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE 6 and older
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+}
+httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status == 200) {
+        returnedData = httpRequest.responseText;
+        var data = JSON.parse(returnedData);
+        if (data.hasOwnProperty('results')) {
+            var dates = data.results.map(function (result) {
+                return result.date;
+            });
+            console.log('dates: ', dates);
+        }
+    } else {
+        // still not ready or error occurred
+    }
+};
+httpRequest.open('GET', url, true);
+httpRequest.send(null);
+
+
+
 app.listen(app.get('port'), () => console.log('listening to: ' + app.get('port')));
 
 // NASA API ssJOmcyAlMslRMBklollwmpbUSmejdcgJlsemAzo
