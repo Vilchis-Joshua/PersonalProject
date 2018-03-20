ab34b9bf4d3618e56ce277b58dbbf00bedbe4a46
jsvilchis <vil15009@byui.edu>
2018-03-20 16:22:08 -0400

    fixed

--- public/javascript/teamActivity11.js
+++ public/javascript/teamActivity11.js
@@ -1,6 +1,6 @@
 function searchInfo() {
     var search = $('#movieSearch').val();
-    var x = $(document).ready(() => {
+    $(document).ready(() => {
         $('btn1').click(() => {
             console.log('Inside function with val: ' + $('#movieSearch').val())
             console.log('Inside function with text: ' + $('#movieSearch').text())
