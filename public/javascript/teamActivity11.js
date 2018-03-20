009d5935e17d7a5a7a03279cffaae564d278a529
jsvilchis <vil15009@byui.edu>
2018-03-20 16:24:08 -0400

    line 3-9 not working for some reason

--- public/javascript/teamActivity11.js
+++ public/javascript/teamActivity11.js
@@ -1,11 +1,11 @@
 function searchInfo() {
     var search = $('#movieSearch').val();
-    $(document).ready(() => {
-        $('btn1').click(() => {
-            console.log('Inside function with val: ' + $('#movieSearch').val())
-            console.log('Inside function with text: ' + $('#movieSearch').text())
+    //$(document).ready(() => {
+    //    $('btn1').click(() => {
+    //        console.log('Inside function with val: ' + $('#movieSearch').val())
+    //        console.log('Inside function with text: ' + $('#movieSearch').text())
 
-        })
-    })
+    //    })
+    //})
     console.log('Search: ' + search);
 }
