02b2bb2b91eda6da54c1caefafd467f1c5c87bf4
jsvilchis <vil15009@byui.edu>
2018-03-20 16:21:02 -0400

    debugging

--- public/javascript/teamActivity11.js
+++ public/javascript/teamActivity11.js
@@ -1,6 +1,11 @@
 function searchInfo() {
+    var search = $('#movieSearch').val();
     var x = $(document).ready(() => {
-        $('#movieSearch').text();
+        $('btn1').click(() => {
+            console.log('Inside function with val: ' + $('#movieSearch').val())
+            console.log('Inside function with text: ' + $('#movieSearch').text())
+
+        })
     })
     console.log('Search: ' + x);
 }
