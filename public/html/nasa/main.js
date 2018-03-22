b4db236105af68c8957edc03fd3fc353e97367bd
jsvilchis <vil15009@byui.edu>
2018-03-22 16:46:58 -0400

    oops....

--- public/html/nasa/main.js
+++ public/html/nasa/main.js
@@ -6,6 +6,17 @@ var photoDate = $('#thisDate').val();
 
 $(document).ready(function () {
     $(':button').click(function () {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+        today = yyyy + '-' + mm + '-' + dd;
         $.ajax({
             url: url + today + apiKey,
             success: function (result) {
