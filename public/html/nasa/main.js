b6ea01eba29a5b7d96b2317e9af56a6201048978
jsvilchis <vil15009@byui.edu>
2018-03-22 20:30:12 -0400

    had a typo

--- public/html/nasa/main.js
+++ public/html/nasa/main.js
@@ -53,7 +53,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $('#savePic').click(function () {
-        if (localStorage == 0) {
+        if (localStorage.length == 0) {
             var key = 'saved 1';
             localStorage.setItem(key, picLink)
         } else {
