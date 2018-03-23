40bea0a9a39a49a43911c3ead3d7e44e38811be5
jsvilchis <vil15009@byui.edu>
2018-03-22 20:28:52 -0400

    why no work?

--- public/html/nasa/main.js
+++ public/html/nasa/main.js
@@ -43,6 +43,7 @@ $(document).ready(function () {
                 $("#main_title").text(result.title);
                 $("returnObject").hide();
                 picLink = result.url;
+                console.log(picLink);
             }
         });
     })
