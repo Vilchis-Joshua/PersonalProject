18834f866e06f1b194ffbef48f4d65613da185d7
jsvilchis <vil15009@byui.edu>
2018-03-22 20:25:40 -0400

    trying to save and display

--- public/html/nasa/saved.js
+++ public/html/nasa/saved.js
@@ -1,6 +1,15 @@
 function startUp() {
+    
     if (localStorage.length == 0) {
         var text = 'You have no saved photos yet!';
         document.getElementById('display').innerHTML = text;
+    } else {
+        for (var i = 0; i < localStorage.length; i++) {
+            //var image = document.createElement('img');
+            var image = $('img');
+            var text = localStorage.getItem(i);
+            image.attr('id', text);
+            $("#displaySaved").append(image);
+        }
     }
 }
