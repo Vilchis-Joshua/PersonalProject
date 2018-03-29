fc11eaf03a5bf3e4e496e481e6c958a9693f2860
jsvilchis <vil15009@byui.edu>
2018-03-28 21:17:00 -0400

    trying with jquery

--- public/html/nasa/saved.js
+++ public/html/nasa/saved.js
@@ -1,20 +1,35 @@
-function startUp() {
+//function startUp() {
     
+//    if (localStorage.length == 0) {
+//        var text = 'You have no saved photos yet!';
+//        document.getElementById('display').innerHTML = text;
+//    } else {
+//        for (var i = 0; i < localStorage.length; i++) {
+//            //var image = document.createElement('img');
+//            var image = document.createElement('img');
+//            image.setAttribute('id', ('image ' + 1));
+//            var saved = localStorage.getItem((i + 1));
+//            console.log('saved: ' + saved);
+//            image.src = saved;
+//            image.setAttribute('width', '250px');
+//            image.setAttribute('height', '250px');
+//            console.log(image.src);
+//            document.getElementById('displaySaved').appendChild(image)
+//        }
+//    }
+//}
+
+
+$(document).ready(function () {
     if (localStorage.length == 0) {
-        var text = 'You have no saved photos yet!';
-        document.getElementById('display').innerHTML = text;
+        $('#display').text('You have no saved photos yet');
     } else {
         for (var i = 0; i < localStorage.length; i++) {
-            //var image = document.createElement('img');
             var image = document.createElement('img');
-            image.setAttribute('id', ('image ' + 1));
-            var saved = localStorage.getItem((i + 1));
-            console.log('saved: ' + saved);
-            image.src = saved;
-            image.setAttribute('width', '250px');
-            image.setAttribute('height', '250px');
-            console.log(image.src);
-            document.getElementById('displaySaved').appendChild(image)
+            image.setAttribute('id', ('image' + i));
+            image.setAttribute('src', localStorage.getItem((i + 1));
+            console.log(image);
+            $('displaySaved').append(image);
         }
     }
-}
+})
